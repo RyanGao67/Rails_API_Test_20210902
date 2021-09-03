39507cfda15b9f34ebe6cf309e51fc4f9896269d
RyanGao67 <tiangao0611@gmail.com>
2021-09-02 23:51:36 -0400

    a

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Build rails api 
 
 [https://www.digitalocean.com/community/tutorials/build-a-restful-json-api-with-rails-5-part-one](https://www.digitalocean.com/community/tutorials/build-a-restful-json-api-with-rails-5-part-one)
+
+
+simple api 
+[https://www.youtube.com/watch?v=QojnRc7SS9o](https://www.youtube.com/watch?v=QojnRc7SS9o)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_022023) do
+ActiveRecord::Schema.define(version: 2021_09_03_031330) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
